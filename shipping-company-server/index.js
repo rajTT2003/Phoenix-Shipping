d73a6151c01589dfa5e28f1a45a6f025d016fa54
rajTT2003 <rajairethomas10@gmail.com>
2025-03-15 06:44:22 -0500

    Fix Vite deployment for Render

--- shipping-company-server/index.js
+++ shipping-company-server/index.js
@@ -13,12 +13,11 @@ const app = express();
 
 const path = require('path');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'shipping-company-client/build')));
 
 // Catch-all route for any other paths (React Router will take over)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'shipping-company-client/build', 'index.html'));
 });
 // Middleware
 app.use(express.json());
